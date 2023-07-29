2d89869d67ca2e9175f1faeec1842bee55d64c67
alln <16016128@qq.com>
2023-07-29 13:01:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ wget https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost-linux-amd64
 gunzip gost-linux-amd64-2.11.1.gz
 mv gost-linux-amd64-2.11.1 /usr/bin/gost
 chmod -R 777 /usr/bin/gost
-nohup gost -L root:asd12345@0.0.0.0:443
+nohup gost -L root:asd12345@0.0.0.0:443 > /dev/null 2>&1 &
